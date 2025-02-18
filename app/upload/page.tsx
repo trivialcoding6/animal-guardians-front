7f078eb5caa1093b94168d2a1e4d09d051b37162
Eatcoding <trivialcoding6@gmail.com>
2025-02-18 20:01:36 +0900

    home 화면 추가

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/service/Header";
 import RecommendSection from "@/components/service/RecommendSection";
 import ResultSection from "@/components/service/ResultSection";
 import UploadSection from "@/components/service/UploadSection";
@@ -6,7 +5,6 @@ import UploadSection from "@/components/service/UploadSection";
 function Upload() {
   return (
     <div className="space-y-10">
-      <Header />
       <UploadSection />
       <ResultSection />
       <RecommendSection />
--- components/service/Header.tsx
+++ components/service/Header.tsx
@@ -1,9 +1,9 @@
-import { Stethoscope } from "lucide-react";
+import Image from "next/image";
 
 function Header() {
   return (
     <header className="flex items-center gap-2 bg-white p-2">
-      <Stethoscope className="w-10 h-10" />
+      <Image src="/images/logo.png" alt="logo" width={50} height={50} />
       <h1 className="text-xl font-bold">반려견 피부질환 진단 서비스</h1>
     </header>
   );

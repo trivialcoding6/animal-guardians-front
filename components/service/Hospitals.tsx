4c84890540e966f0ae52be4dd6969852aadae3e3
Eatcoding <trivialcoding6@gmail.com>
2025-02-25 17:23:03 +0900

    해마루 http 처리

--- components/service/Hospitals.tsx
+++ components/service/Hospitals.tsx
@@ -23,30 +23,38 @@ function Hospitals({ diseaseType }: Props) {
   return (
     <Recommend title="주변 동물병원">
       <ul className="space-y-4">
-        {disease.hospitals.map((hospital, index) => (
-          <li key={index} className="p-4 border rounded-lg shadow-sm">
-            <h3 className="flex items-center gap-2 text-md font-semibold">
-              <Building2 className="w-5 h-5 text-purple-500" />
-              {hospital.hospital_name}
-            </h3>
-            <p className="mt-2 flex items-center gap-2 text-gray-600">
-              <MapPin className="w-5 h-5 text-blue-500" />
-              {hospital.address}
-            </p>
-            <p className="flex items-center gap-2 text-gray-600">
-              <Phone className="w-5 h-5 text-green-500" />
-              {hospital.contact_info}
-            </p>
-            <Link
-              href={`https://${hospital.website}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block mt-3 px-4 py-2 bg-indigo-500 text-white rounded-md text-sm hover:bg-indigo-600 transition-colors"
-            >
-              사이트 바로가기
-            </Link>
-          </li>
-        ))}
+        {disease.hospitals.map((hospital, index) => {
+          const isHamaluru = hospital.website.includes("해마루");
+
+          return (
+            <li key={index} className="p-4 border rounded-lg shadow-sm">
+              <h3 className="flex items-center gap-2 text-md font-semibold">
+                <Building2 className="w-5 h-5 text-purple-500" />
+                {hospital.hospital_name}
+              </h3>
+              <p className="mt-2 flex items-center gap-2 text-gray-600">
+                <MapPin className="w-5 h-5 text-blue-500" />
+                {hospital.address}
+              </p>
+              <p className="flex items-center gap-2 text-gray-600">
+                <Phone className="w-5 h-5 text-green-500" />
+                {hospital.contact_info}
+              </p>
+              <Link
+                href={
+                  isHamaluru
+                    ? `https://${hospital.website}`
+                    : `http://${hospital.website}`
+                }
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block mt-3 px-4 py-2 bg-indigo-500 text-white rounded-md text-sm hover:bg-indigo-600 transition-colors"
+              >
+                사이트 바로가기
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </Recommend>
   );
